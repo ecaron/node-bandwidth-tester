09999c25171c4b84ce52ff5bf680c5030d56b9bd
Eric Caron <eric.caron@gmail.com>
2016-09-18 22:53:50 -0500

    Initial (very raw) commit of a basic client package that measures latency, download speed and ping time response

---
+++ README.md
@@ -0,0 +1,7 @@
+http://speedtest.sjc01.softlayer.com/downloads/test100.zip
+  data: 
+   { url: 'http://speedtest.sjc01.softlayer.com/downloads/test100.zip?cacheBuster=1474255689839',
+     dataSizeKB: 98616.7265625,
+     latency: 4,
+     throughput: 3323.36 } }
+
---
+++ client.js
@@ -0,0 +1,21 @@
+'use strict';
+
+require('dotenv').config();
+
+if (typeof process.env.PING_REMOTE === 'undefined' || typeof process.env.TARGET_FILE === 'undefined') {
+	console.warn('PING_REMOTE and/or TARGET_FILE not defined in .env. See sample.env for an example');
+	process.exit();
+
+}
+
+var async = require('async');
+var request = require('request');
+var bandwidthTest = require('./lib/bandwidth');
+var pingTest = require('./lib/ping');
+
+async.series({
+	bandwidth: bandwidthTest,
+	ping: pingTest
+}, function(err, results) {
+	console.log(results);
+});
