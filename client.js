e1fdd361740bd807faed65e7cc31be7b73f3ad9e
Eric Caron <eric.caron@gmail.com>
2016-09-20 06:49:48 -0500

    Scripts are now semistandard-friendly, and bandwidth test now reports min & max (before was just average)

--- client.js
+++ client.js
@@ -3,9 +3,8 @@
 require('dotenv').config();
 
 if (typeof process.env.PING_REMOTE === 'undefined' || typeof process.env.TARGET_FILE === 'undefined') {
-	console.warn('PING_REMOTE and/or TARGET_FILE not defined in .env. See sample.env for an example');
-	process.exit();
-
+  console.warn('PING_REMOTE and/or TARGET_FILE not defined in .env. See sample.env for an example');
+  process.exit();
 }
 
 var async = require('async');
@@ -14,8 +13,10 @@ var bandwidthTest = require('./lib/bandwidth');
 var pingTest = require('./lib/ping');
 
 async.series({
-	bandwidth: bandwidthTest,
-	ping: pingTest
-}, function(err, results) {
-	console.log(results);
+  bandwidth: bandwidthTest,
+  ping: pingTest
+}, function (err, results) {
+  if (err) console.warn(err);
+  typeof request;
+  console.log(results);
 });
